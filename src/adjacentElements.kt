975a2641fccba083fae23882f0c207d291938226
ali <abduallah@email.com>
2021-07-25 17:37:38 +0300

    Initial commit

---
+++ src/adjacentElements.kt
@@ -0,0 +1,14 @@
+//Given an array of integers, find the pair of adjacent
+//elements that has the largest product and return that product.
+
+//Example
+
+//For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+
+//adjacentElementsProduct(inputArray) = 21.
+
+//7 and 3 produce the largest product.
+
+fun adjacentElementsProduct(inputArray: MutableList<Int>): Int = (0..inputArray.size-2).map{
+    inputArray[it] * inputArray[it + 1]
+}.max()?: Integer.MIN_VALUE
